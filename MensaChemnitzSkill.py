854e8e64eb4cc2f5766edac74c5c285dcb9c5e64
Stephan Ruttloff <stephan@ruttloff.org>
2017-03-12 14:43:57 +0100

    get meals for a specific date

--- MensaChemnitzSkill.py
+++ MensaChemnitzSkill.py
@@ -23,24 +23,45 @@ def stop():
 @ask.intent('GetMenu')
 def getMeals():
   date = datetime.now()
-  canteens = MensaApi.getCanteenMeals(date)
+  return getMealsDate(date)
 
-  total_meal_count = len([m.meals for c in canteens for m in c.meals])
-  if total_meal_count == 0:
-    return statement(render_template('meals_today_none'))
+@ask.intent('GetMenuDate', convert={'date': 'date'})
+def getMealsDate(date):
+  date = datetime.combine(date, datetime.min.time())
+  canteens = list(MensaApi.getCanteenMeals(date))
+  return respond(date, canteens)
+
+def renderMeals(canteen):
+  return "<break strength='medium'/>oder<break strength='medium'/>".join([m.description for m in canteen.meals]).strip()
+
+def renderDate(date):
+  return "<say-as interpret-as='date'>" + date.strftime("????%m%d") + "</say-as>"
+
+def respond(date, canteens):
+  meals = [meal for canteen in canteens for meal in canteen.meals]
+
+  if len(meals) == 0:
+    return statement(render_template('meals_none', date=renderDate(date)))
   else:
-    responses = []
-    for canteen in canteens:
+    first_canteen = canteens.pop(0)
+    remaining_canteens = canteens
+    remaining_rendered = []
+
+    for canteen in remaining_canteens:
+      print("remaining", canteen.name)
       if len(canteen.meals) == 0:
-        rendered = render_template('meals_today_none_canteen', canteen=canteen.name)
-        responses.append("<p>" + rendered + "</p>")
+        print("none")
+        rendered = render_template('meals_remaining_none', canteen=canteen.name)
+        remaining_rendered.append("<p>" + rendered + "</p>")
       else:
-        meals_string = "<break strength='medium'/>oder<break strength='medium'/>".join([m.description for m in canteen.meals]).strip()
-        rendered = render_template('meals_today', canteen=canteen.name, meals=meals_string)
-        responses.append("<p>" + rendered + "</p>")
-    response = "".join(responses)
-    response = "<speak>" + response + "</speak>"
-    return statement(response)
+        print("not none")
+        meals_string = renderMeals(canteen)
+        rendered = render_template('meals_remaining', canteen=canteen.name, meals=meals_string)
+        remaining_rendered.append("<p>" + rendered + "</p>")
+
+    remaining = "".join(remaining_rendered)
+    first = render_template('meals_first', date=renderDate(date), canteen=first_canteen.name, meals=renderMeals(first_canteen))
+    return statement("<speak>" + first + remaining + "</speak>")
 
 if __name__ == '__main__':
   app.run(debug=True)
